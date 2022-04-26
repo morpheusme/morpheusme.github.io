c1f3d88516fe6653fdb67e490826c3733fbd5cc3
Muhammad Q Shahzad <mqasims@live.com>
2022-04-26 17:46:42 -0400

    new portfolio

---
+++ precache-manifest.68a0ad2f98372320b00268107b05b3ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13efc0a85516404e572bdd9a04decf94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae0920d85d73c6b2cabd",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "afe83c007f0b24c7abaa",
+    "url": "/static/js/2.b0986d47.chunk.js"
+  },
+  {
+    "revision": "ae0920d85d73c6b2cabd",
+    "url": "/static/js/main.def53f1c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
